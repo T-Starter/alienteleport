082482b490480cfca4cc5633db07222918581904
Dewald928 <dewald123@rocketmail.com>
2021-08-19 19:31:09 +0200

    example update

--- oracle/config-example.js
+++ oracle/config-example.js
@@ -5,15 +5,15 @@ module.exports = {
     eos: {
         wsEndpoint: 'ws://178.63.44.179:8082',
         chainId: "f16b1833c747c43682f4386fca9cbb327929334a762755ebec17f6f23c9b8a12",
-        endpoint: 'https://testnet.waxsweden.org',
-        teleportContract: 'other.worlds',
+        endpoint: 'https://testnet.eos.miami',
+        teleportContract: 'tport.start',
         oracleAccount: '',
         privateKey: '5C234dc...'
     },
     eth: {
-        wsEndpoint: 'wss://kovan.infura.io/ws/v3/9ae811d1c04243f2869a05848207b985',
-        endpoint: 'https://ropsten.infura.io/v3/9ae811d1c04243f2869a05848207b985',
-        teleportContract: '0x0789a30aa30d6a7a7536a7ed26956bbdb0fc80bd',
+        wsEndpoint: 'wss://ropsten.infura.io/ws/v3/070239498bd64e588d26301a8c2ef6e6',
+        endpoint: 'https://ropsten.infura.io/v3/070239498bd64e588d26301a8c2ef6e6',
+        teleportContract: '0x5b6a8e651c4083177851d34409685977ae21e4ca',
         oracleAccount: '0x111111111111111111111111111111111111111',
         privateKey: 'ABC434DCF...'
     }
