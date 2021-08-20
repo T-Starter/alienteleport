5ccff7a79b3c9686695e7ef5565ed96651a73668
Dewald928 <dewald123@rocketmail.com>
2021-08-20 12:30:02 +0200

    eos reporter async

--- oracle/reporter-eos.js
+++ oracle/reporter-eos.js
@@ -32,103 +32,117 @@ const fromHexString = (hexString) =>
 const toHexString = (bytes) =>
   bytes.reduce((str, byte) => str + byte.toString(16).padStart(2, "0"), "");
 
-const run = async () => {
-  const rows = [];
-  console.log("Fetching all rows form teleports table");
-  while (true) {
-    const res = await rpc.get_table_rows({
-      code: config.eos.teleportContract,
-      scope: config.eos.teleportContract,
-      table: "teleports",
-      limit: 100,
-    });
-
-    // console.log(res);
-
-    res.rows.forEach((r) => {
-      if (r.signatures.length < 3) {
-        rows.push(r);
+const sleep = (time = 0, throwError = false) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (throwError) {
+        reject();
+      } else {
+        resolve();
       }
-    });
+    }, time);
+  });
+};
 
-    if (res.more) {
-      lower_bound = res.next_key;
-    } else {
-      break;
-    }
-  }
-
-  console.log("Teleports: ", rows);
-
-  for (const teleport of rows) {
-    if (
-      !teleport.claimed &&
-      !teleport.oracles.includes(config.eos.oracleAccount)
-    ) {
-      console.log(
-        `Reporting ${teleport.account} - ${
-          teleport.id
-        }, oracles : ${JSON.stringify(teleport.oracles)}, amount : ${
-          teleport.quantity
-        }`
-      );
-
-      // eth signing
-      // sign the transaction and send to the eos chain
-
-      const sb = new Serialize.SerialBuffer({
-        textEncoder: new TextEncoder(),
-        textDecoder: new TextDecoder(),
+const run = async () => {
+  while (true) {
+    try {
+      const rows = [];
+      console.log("Fetching all rows form teleports table");
+      const res = await rpc.get_table_rows({
+        code: config.eos.teleportContract,
+        scope: config.eos.teleportContract,
+        table: "teleports",
+        limit: 1000,
+        reverse: true,
       });
-      sb.pushNumberAsUint64(teleport.id);
-      sb.pushUint32(teleport.time);
-      sb.pushName(teleport.account);
-      sb.pushAsset(teleport.quantity);
-      sb.push(teleport.chain_id);
-      sb.pushArray(fromHexString(teleport.eth_address));
 
-      const data_buf = Buffer.from(sb.array.slice(0, 69));
-      const msg_hash = ethUtil.keccak(data_buf);
-      console.log(msg_hash.toString('hex'));
-      console.log(config.eth.privateKey);
-      const pk = Buffer.from(config.eth.privateKey, "hex");
-      const sig = ethUtil.ecsign(msg_hash, pk);
-      console.log(pk, sig);
+      // console.log(res);
 
-      const signature = ethUtil.toRpcSig(sig.v, sig.r, sig.s);
-      console.log(`Created signature ${signature}`);
-
-      // sign eos transaction
-      const actions = [
-        {
-          account: config.eos.teleportContract,
-          name: "sign",
-          authorization: [
-            {
-              actor: config.eos.oracleAccount,
-              permission: config.eos.oraclePermission || "active",
-            },
-          ],
-          data: {
-            oracle_name: config.eos.oracleAccount,
-            id: teleport.id,
-            signature: signature,
-          },
-        },
-      ];
-
-      console.log(actions);
-      const res = await eos_api.transact(
-        { actions },
-        {
-          blocksBehind: 3,
-          expireSeconds: 180,
+      res.rows.forEach((r) => {
+        if (r.signatures.length < 1) {
+          rows.push(r);
         }
-      );
-      console.log(res);
+      });
+
+      console.log("Teleports: ", rows);
+
+
+      for (const teleport of rows) {
+        if (
+          !teleport.claimed &&
+          !teleport.oracles.includes(config.eos.oracleAccount)
+        ) {
+          console.log(
+            `Reporting ${teleport.account} - ${
+              teleport.id
+            }, oracles : ${JSON.stringify(teleport.oracles)}, amount : ${
+              teleport.quantity
+            }`
+          );
+
+          // eth signing
+          // sign the transaction and send to the eos chain
+
+          const sb = new Serialize.SerialBuffer({
+            textEncoder: new TextEncoder(),
+            textDecoder: new TextDecoder(),
+          });
+          sb.pushNumberAsUint64(teleport.id);
+          sb.pushUint32(teleport.time);
+          sb.pushName(teleport.account);
+          sb.pushAsset(teleport.quantity);
+          sb.push(teleport.chain_id);
+          sb.pushArray(fromHexString(teleport.eth_address));
+
+          const data_buf = Buffer.from(sb.array.slice(0, 69));
+          const msg_hash = ethUtil.keccak(data_buf);
+          console.log(msg_hash.toString("hex"));
+          console.log(config.eth.privateKey);
+          const pk = Buffer.from(config.eth.privateKey, "hex");
+          const sig = ethUtil.ecsign(msg_hash, pk);
+          console.log(pk, sig);
+
+          const signature = ethUtil.toRpcSig(sig.v, sig.r, sig.s);
+          console.log(`Created signature ${signature}`);
+
+          // sign eos transaction
+          const actions = [
+            {
+              account: config.eos.teleportContract,
+              name: "sign",
+              authorization: [
+                {
+                  actor: config.eos.oracleAccount,
+                  permission: config.eos.oraclePermission || "active",
+                },
+              ],
+              data: {
+                oracle_name: config.eos.oracleAccount,
+                id: teleport.id,
+                signature: signature,
+              },
+            },
+          ];
+
+          console.log(actions);
+          const res = eos_api.transact(
+            { actions },
+            {
+              blocksBehind: 3,
+              expireSeconds: 180,
+            }
+          );
+          console.log(res);
+        }
+      }
+      console.log("Done");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      await sleep(10000);
     }
   }
-  console.log("Done");
 };
 
 run();
