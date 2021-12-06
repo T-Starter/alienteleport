3be2245726cb4f385d75c2da5ef34239fdd816a1
Dewald928 <dewald123@rocketmail.com>
2021-12-06 15:04:15 +0200

    re-optimize reporters

--- oracle/config-example.js
+++ oracle/config-example.js
@@ -1,6 +1,7 @@
 module.exports = {
     network: 'BSC', // ETH, TELOS, BSC
     chainId: '2',
+    pollingInterval: 30000,
     eos: {
         wsEndpoint: 'ws://178.63.44.179:8082',
         chainId: "f16b1833c747c43682f4386fca9cbb327929334a762755ebec17f6f23c9b8a12",
@@ -10,8 +11,8 @@ module.exports = {
         privateKey: '5C234dc...'
     },
     eth: {
-        wsEndpoint: 'wss://ropsten.infura.io/ws/v3/070239498bd64e588d26301a8c2ef6e6',
-        endpoint: 'https://ropsten.infura.io/v3/070239498bd64e588d26301a8c2ef6e6',
+        wsEndpoint: 'wss://ropsten.infura.io/ws/v3',
+        endpoint: 'https://ropsten.infura.io/v3',
         oracleAccount: '0x111111111111111111111111111111111111111',
         privateKey: 'ABC434DCF...'
     }
--- oracle/reporter-eth.js
+++ oracle/reporter-eth.js
@@ -133,73 +133,76 @@ const process_claimed = async (events) => {
             });
 
             let tokensList = tokensTable.rows.filter(token => token.enabled == 1);
-            let tokenAddrList = [...new Set(tokensList.map(r => r.remote_contracts).flat().map(r => r.value))];
-            // console.log(tokensList);
+            let tokenAddrList = [...new Set(tokensList.map(r => r.remote_contracts).flat().filter(t => t.key == config.chainId).map(r => r.value.toLowerCase()))];
+            // console.log(tokenAddrList);                
 
-            for (const tokenAddress of tokenAddrList){
-                // find token from tokenAddress in tokensList
-                const token = tokensList.find(token => token.remote_contracts.find(remote => remote.value == tokenAddress));
-                const tokenPrecision = getDecimalFromAsset(token.token);
-                const tokenSymbol = getSymFromAsset(token.token);
-
-                if (events.length){
-                    for (let r = 0; r < events.length; r++){
-                        let data;
-                        if (events[r].topics[0] == claimed_topic){
-                            data = await ethers.utils.defaultAbiCoder.decode([ 'uint64', 'address', 'uint' ], events[r].data);
-                        } else {
-                            continue;
-                        }
-                        // console.log(data)
-                        // console.log(events[r], data, data[1].toString());
-                        const id = data[0].toNumber();
-                        const to_eth = data[1].replace('0x', '') + '000000000000000000000000';
-                        const quantity = (data[2].toNumber() / Math.pow(10, tokenPrecision)).toFixed(tokenPrecision) + ' ' + tokenSymbol;
-                        const actions = [];
-                        actions.push({
-                            account: config.eos.teleportContract,
-                            name: 'claimed',
-                            authorization: [{
-                                actor: config.eos.oracleAccount,
-                                permission: config.eos.oraclePermission || 'active'
-                            }],
-                            data: {
-                                oracle_name: config.eos.oracleAccount,
-                                id,
-                                to_eth,
-                                quantity
-                            }
-                        });
-                        // console.log(actions, events[r].transactionHash);
-
-                        await_confirmation(events[r].transactionHash).then(async () => {
-                            try {
-                                const eos_res = await eos_api.transact({actions}, {
-                                    blocksBehind: 3,
-                                    expireSeconds: 180,
-                                });
-                                console.log(`Sent notification of claim with txid ${eos_res.transaction_id}, for ID ${id}, account 0x${to_eth.substr(0, 40)}, quantity ${quantity}`);
-                                // resolve();
-                            }
-                            catch (e){
-                                if (e.message.indexOf('Already marked as claimed') > -1){
-                                    console.log(`ID ${id} is already claimed, account 0x${to_eth.substr(0, 40)}, quantity ${quantity}`);
-                                }
-                                else {
-                                    console.error(`Error sending confirm ${e.message}`);
-                                    // reject(e);
-                                }
-                            }
-                        });
-
-                        await sleep(500);
+            if (events.length){
+                for (let r = 0; r < events.length; r++){
+                    if (!tokenAddrList.includes(events[r].address.toLowerCase())){
+                        console.log(`Token ${events[r].address.toLowerCase()} not found in tokens table`);
+                        continue;
                     }
-                }
 
-                resolve();
+                    // find token from tokenAddress in tokensList
+                    const token = tokensList.find(token => token.remote_contracts.find(remote => remote.value.toLowerCase() == events[r].address.toLowerCase()));
+                    const tokenPrecision = getDecimalFromAsset(token.token);
+                    const tokenSymbol = getSymFromAsset(token.token);                
+                
+                    let data;
+                    if (events[r].topics[0] == claimed_topic){
+                        data = await ethers.utils.defaultAbiCoder.decode([ 'uint64', 'address', 'uint' ], events[r].data);
+                    } else {
+                        continue;
+                    }
+                    // console.log(data)
+                    // console.log(events[r], data, data[1].toString());
+                    const id = data[0].toNumber();
+                    const to_eth = data[1].replace('0x', '') + '000000000000000000000000';
+                    const quantity = (data[2].toNumber() / Math.pow(10, tokenPrecision)).toFixed(tokenPrecision) + ' ' + tokenSymbol;
+                    const actions = [];
+                    actions.push({
+                        account: config.eos.teleportContract,
+                        name: 'claimed',
+                        authorization: [{
+                            actor: config.eos.oracleAccount,
+                            permission: config.eos.oraclePermission || 'active'
+                        }],
+                        data: {
+                            oracle_name: config.eos.oracleAccount,
+                            id,
+                            to_eth,
+                            quantity
+                        }
+                    });
+                    // console.log(actions, events[r].transactionHash);
+
+                    await_confirmation(events[r].transactionHash).then(async () => {
+                        try {
+                            const eos_res = await eos_api.transact({actions}, {
+                                blocksBehind: 3,
+                                expireSeconds: 180,
+                            });
+                            console.log(`Sent notification of claim with txid ${eos_res.transaction_id}, for ID ${id}, account 0x${to_eth.substr(0, 40)}, quantity ${quantity}`);
+                            // resolve();
+                        }
+                        catch (e){
+                            if (e.message.indexOf('Already marked as claimed') > -1){
+                                console.log(`ID ${id} is already claimed, account 0x${to_eth.substr(0, 40)}, quantity ${quantity}`);
+                            }
+                            else {
+                                console.error(`Error sending confirm ${e.message}`);
+                                // reject(e);
+                            }
+                        }
+                    });
+
+                    await sleep(500);
+                }
             }
 
-        }
+            resolve();
+            }
+            
         catch (e){
             reject(e);
         }
@@ -219,86 +222,89 @@ const process_teleported = async (events) => {
             });
 
             let tokensList = tokensTable.rows.filter(token => token.enabled == 1);
-            let tokenAddrList = [...new Set(tokensList.map(r => r.remote_contracts).flat().map(r => r.value))];
+            let tokenAddrList = [...new Set(tokensList.map(r => r.remote_contracts).flat().filter(t => t.key == config.chainId).map(r => r.value.toLowerCase()))];
             // console.log(tokensList);
 
-            for (const tokenAddress of tokenAddrList){
-                // find token from tokenAddress in tokensList
-                const token = tokensList.find(token => token.remote_contracts.find(remote => remote.value == tokenAddress));
-                const tokenPrecision = getDecimalFromAsset(token.token);
-                const tokenSymbol = getSymFromAsset(token.token);
-
-                if (events.length){
-                    for (let r = 0; r < events.length; r++){
-                        let data;
-                        if (events[r].topics[0] == teleport_topic){
-                            data = await ethers.utils.defaultAbiCoder.decode([ 'string', 'uint', 'uint' ], events[r].data);
-                        } else {
-                            continue;
-                        }
-
-                        // console.log(events[r], data, data[1].toString())
-
-                        const tokens = data[1].toNumber();
-                        if (tokens <= 0){
-                            // console.error(data);
-                            console.error('Tokens are less than or equal to 0');
-                            continue;
-                        }
-                        const to = data[0];
-                        const from_chain_id = config.chainId;
-                        const to_chain_id = data[2].toNumber();
-                        const amount = (tokens / Math.pow(10, tokenPrecision)).toFixed(tokenPrecision);
-                        const quantity = `${amount} ${tokenSymbol}`
-                        const txid = events[r].transactionHash.replace(/^0x/, '');
-
-                        const actions = [];
-                        actions.push({
-                            account: config.eos.teleportContract,
-                            name: 'received',
-                            authorization: [{
-                                actor: config.eos.oracleAccount,
-                                permission: config.eos.oraclePermission || 'active'
-                            }],
-                            data: {
-                                oracle_name: config.eos.oracleAccount,
-                                to,
-                                ref: txid,
-                                quantity,
-                                from_chain_id,
-                                to_chain_id,
-                                confirmed: true
-                            }
-                        });
-                        // console.log(actions);
-
-                        await_confirmation(events[r].transactionHash).then(async () => {
-                            try {
-                                const eos_res = await eos_api.transact({actions}, {
-                                    blocksBehind: 3,
-                                    expireSeconds: 180,
-                                });
-                                console.log(`Sent notification of teleport with txid ${eos_res.transaction_id}`);
-                                // resolve();
-                            }
-                            catch (e){
-                                if (e.message.indexOf('Oracle has already approved') > -1){
-                                    console.log('Oracle has already approved');
-                                }
-                                else {
-                                    console.error(`Error sending teleport ${e.message}`);
-                                    // reject(e);
-                                }
-                            }
-                        });
-
-                        await sleep(500);
+            if (events.length){
+                for (let r = 0; r < events.length; r++){
+                    if (!tokenAddrList.includes(events[r].address.toLowerCase())){
+                        console.log(`Token ${events[r].address.toLowerCase()} not found in tokens table`);
+                        continue;
                     }
-                }
 
-                resolve();
+                    // find token from tokenAddress in tokensList
+                    const token = tokensList.find(token => token.remote_contracts.find(remote => remote.value.toLowerCase() == events[r].address.toLowerCase()));
+                    const tokenPrecision = getDecimalFromAsset(token.token);
+                    const tokenSymbol = getSymFromAsset(token.token);    
+
+                    let data;
+                    if (events[r].topics[0] == teleport_topic){
+                        data = await ethers.utils.defaultAbiCoder.decode([ 'string', 'uint', 'uint' ], events[r].data);                            
+                    } else {
+                        continue;
+                    }
+
+                    // console.log(events[r], data, data[1].toString())
+
+                    const tokens = data[1].toNumber();
+                    if (tokens <= 0){
+                        // console.error(data);
+                        console.error('Tokens are less than or equal to 0');
+                        continue;
+                    }
+                    const to = data[0];
+                    const from_chain_id = config.chainId;
+                    const to_chain_id = data[2].toNumber();
+                    const amount = (tokens / Math.pow(10, tokenPrecision)).toFixed(tokenPrecision);
+                    const quantity = `${amount} ${tokenSymbol}`
+                    const txid = events[r].transactionHash.replace(/^0x/, '');
+
+                    const actions = [];
+                    actions.push({
+                        account: config.eos.teleportContract,
+                        name: 'received',
+                        authorization: [{
+                            actor: config.eos.oracleAccount,
+                            permission: config.eos.oraclePermission || 'active'
+                        }],
+                        data: {
+                            oracle_name: config.eos.oracleAccount,
+                            to,
+                            ref: txid,
+                            quantity,
+                            from_chain_id,
+                            to_chain_id,
+                            confirmed: true
+                        }
+                    });
+                    // console.log(actions);
+
+                    await_confirmation(events[r].transactionHash).then(async () => {
+                        try {
+                            const eos_res = await eos_api.transact({actions}, {
+                                blocksBehind: 3,
+                                expireSeconds: 180,
+                            });
+                            console.log(`Sent notification of teleport with txid ${eos_res.transaction_id}`);
+                            // resolve();
+                        }
+                        catch (e){
+                            if (e.message.indexOf('Oracle has already approved') > -1){
+                                console.log('Oracle has already approved');
+                            }
+                            else {
+                                console.error(`Error sending teleport ${e.message}`);
+                                // reject(e);
+                            }
+                        }
+                    });
+
+                    await sleep(500);
+                }
             }
-        }
+
+            resolve();
+            }
         catch (e){
             reject(e);
         }
@@ -309,7 +315,7 @@ const run = async (from_block = 'latest') => {
     while (true){
         try {
             const block = await provider.getBlock('latest');
-            console.log(`Fetching events from block ${block}`);
+            console.log(`Fetching events from block ${block.number}`);
             const latest_block = block.number;
 
             if (from_block === 'latest'){
